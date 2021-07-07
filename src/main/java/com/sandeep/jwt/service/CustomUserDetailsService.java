1056d38b56d3d194258dd40353021ab87812b5b0
abc <abc@example.com>
2021-07-07 16:11:06 +0530

    initial commit

---
+++ src/main/java/com/sandeep/jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.sandeep.jwt.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sandeep.jwt.mlodel.User;
+import com.sandeep.jwt.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUserName(username);
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), new ArrayList<>());
+    }
+}
---
+++ src/test/java/com/sandeep/jwt/SbSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
